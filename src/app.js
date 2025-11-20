cef4f71415a215220d5649e988867a6b06197e94
Jules Girardon <jules.grdn@gmail.com>
2025-11-20 14:25:43 +0100

    [init] Configure initial project setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+main().catch((err) => console.log(err));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
